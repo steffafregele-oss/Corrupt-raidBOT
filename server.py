105b069ac950bff0f922b3931c345b63aa9b7d2c
steffafregele-oss <steffafregele@gmail.com>
2025-11-04 16:21:38 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,15 @@
 from flask import Flask
 from threading import Thread
 
-# Creează serverul Flask
-app = Flask('')
+app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Bot is alive!"  # răspuns simplu pentru uptime check
+    return "Bot is running!"
 
 def run():
-    # Pornește serverul pe toate interfețele, port 8080 (Render necesită port fix)
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=8080)
 
 def keep_alive():
-    # Rulează serverul Flask într-un thread separat, astfel încât botul să continue să funcționeze
     t = Thread(target=run)
     t.start()
